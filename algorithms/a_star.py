8d5bc36788daf8ff2b9dfbd91d11c5b8db976501
YunseoH <122462619+YunseoH@users.noreply.github.com>
2023-11-21 10:40:59 +0800

    Update a_star.py

--- algorithms/a_star.py
+++ algorithms/a_star.py
@@ -1,4 +1,122 @@
-# TODO: Implement A* algorithm
+import pygame
+import math
+from queue import PriorityQueue
 
-## Hint: You must be able to reconstruct the path once the algorithm has finished
-## Hint: You must be able to visualize the algorithm in action i.e call the methods to draw on the screen to visualize the algorithm in action in the astar function
+def h(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return abs(x1 - x2) + abs(y1 - y2)
+
+def reconstruct_path(came_from, current, draw):
+    while current in came_from:
+        current = came_from[current]
+        current.make_path()
+        draw()
+
+def a_star(draw, grid, start, end):
+    count = 0
+    open_set = PriorityQueue()
+    open_set.put((0, count, start))
+    came_from = {}
+    g_score = {spot: float("inf") for row in grid for spot in row}
+    g_score[start] = 0
+    f_score = {spot: float("inf") for row in grid for spot in row}
+    f_score[start] = h(start.get_pos(), end.get_pos())
+
+    open_set_hash = {start}
+
+    while not open_set.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        current = open_set.get()[2]
+        open_set_hash.remove(current)
+
+        if current == end:
+            reconstruct_path(came_from, end, draw)
+            end.make_end()
+            return True
+
+        for neighbor in current.neighbors:
+            temp_g_score = g_score[current] + 1
+
+            if temp_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = temp_g_score
+                f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
+                if neighbor not in open_set_hash:
+                    count += 1
+                    open_set.put((f_score[neighbor], count, neighbor))
+                    open_set_hash.add(neighbor)
+                    neighbor.make_open()
+
+        draw()
+
+        if current != start:
+            current.make_closed()
+
+    return False
+
+class Grid:
+    def __init__(self, rows, width):
+        self.rows = rows
+        self.width = width
+        self.grid = []
+        self.gap = width // rows
+        self.make_grid()
+
+    def make_grid(self):
+        self.grid = []
+        for i in range(self.rows):
+            self.grid.append([])
+            for j in range(self.rows):
+                spot = Spot(i, j, self.gap, self.rows)
+                self.grid[i].append(spot)
+
+    def draw(self, win):
+        win.fill((255, 255, 255))  # Fill window with white
+        for row in self.grid:
+            for spot in row:
+                spot.draw(win)
+
+        for i in range(self.rows):
+            pygame.draw.line(win, (128, 128, 128), (0, i * self.gap), (self.width, i * self.gap))
+            for j in range(self.rows):
+                pygame.draw.line(win, (128, 128, 128), (j * self.gap, 0), (j * self.gap, self.width))
+
+    def get_clicked_pos(self, pos):
+        y, x = pos
+        row = y // self.gap
+        col = x // self.gap
+        return row, col
+
+    def reset(self):
+        for row in self.grid:
+            for spot in row:
+                spot.reset()
+
+    def update_neighbors(self):
+        for row in self.grid:
+            for spot in row:
+                spot.update_neighbors(self.grid)
+
+class Spot:
+    def __init__(self, row, col, width, total_rows):
+        self.row = row
+        self.col = col
+        self.x = row * width
+        self.y = col * width
+        self.color = (255, 255, 255)  # White for empty spot
+        self.neighbors = []
+        self.width = width
+        self.total_rows = total_rows
+
+    def get_pos(self):
+        return self.row, self.col
+
+    def is_closed(self):
+        return self.color == (255, 0, 0)  # Red for closed spot
+
+    def is_open(self):
+        return self.color == (0, 255, 0) 
