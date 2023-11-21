fcf08c1fb10e46c2bfce0c3d83839faef25604c9
YunseoH <122462619+YunseoH@users.noreply.github.com>
2023-11-21 10:42:06 +0800

    Update grid.py

--- components/grid.py
+++ components/grid.py
@@ -1,14 +1,41 @@
 import pygame
+from spot import Spot
 
 class Grid:
-    
-    # TODO:
+    def __init__(self, rows, width):
+        self.rows = rows
+        self.width = width
+        self.gap = width // rows
+        self.grid = self._initialize_grid()
 
-    ## Implement the class of the grid
-    ## You must be able to create a grid 
-    ## You must be able to draw the grid
-    ## You must be able to reset the grid
-    ## You must be able to click on the nodes of the grid
-    ## You must also need to implement a method to update the neighbors of each node which builds on top of the previous TODO
-    
-    pass 
+    def _initialize_grid(self):
+        return [[Spot(i, j, self.gap, self.rows) for j in range(self.rows)] for i in range(self.rows)]
+
+    def draw_grid(self, window):
+        window.fill((255, 255, 255))  # White background
+        self._draw_all_spots(window)
+        self._draw_grid_lines(window)
+
+    def _draw_all_spots(self, window):
+        for row in self.grid:
+            for spot in row:
+                spot.draw(window)
+
+    def _draw_grid_lines(self, window):
+        for i in range(self.rows):
+            pygame.draw.line(window, (128, 128, 128), (0, i * self.gap), (self.width, i * self.gap))
+            pygame.draw.line(window, (128, 128, 128), (i * self.gap, 0), (i * self.gap, self.width))
+
+    def handle_click(self, position):
+        row, col = position[1] // self.gap, position[0] // self.gap
+        return self.grid[row][col]
+
+    def reset_grid(self):
+        for row in self.grid:
+            for spot in row:
+                spot.reset()
+
+    def update_grid_neighbors(self):
+        for row in self.grid:
+            for spot in row:
+                spot.update_neighbors(self.grid)
